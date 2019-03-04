5bf37fa1ac5869c3f7f3f07f67bf546ab32c2b0c
Jonathan Walters <jonfwalters@gmail.com>
2019-03-04 20:47:49 +0000

    added route for add recipe

--- app.py
+++ app.py
@@ -16,7 +16,20 @@ def show_recipes():
     return render_template("recipes.html",
     recipes=mongo.db.recipes.find())
 
+@app.route('/add_recipes')
+def add_recipes():
+    return render_template('addrecipes.html',
+    preparation=mongo.db.preparation_time.find(),
+    serv=mongo.db.serves.find(),
+    diff=mongo.db.difficulty.find())
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes=mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('show_recipes'))
+    
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
